af6c76d016f7c0540881c271fbac5f4dac016743
lsm4p5 <lsm4p5@gmail.com>
2023-04-20 23:36:47 +0900

    Initial commit

---
+++ src/main/java/com/uplus/qualityweb/QualitywebApplication.java
@@ -0,0 +1,13 @@
+package com.uplus.qualityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uplus/qualityweb/QualitywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.qualityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
