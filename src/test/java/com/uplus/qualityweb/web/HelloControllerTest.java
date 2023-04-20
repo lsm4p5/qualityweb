f78abdead8970c1a14105947988fac6cec3c5220
lsm4p5 <lsm4p5@gmail.com>
2023-04-21 01:37:13 +0900

    commit

--- src/test/java/com/uplus/qualityweb/web/HelloControllerTest.java
+++ src/test/java/com/uplus/qualityweb/web/HelloControllerTest.java
@@ -2,18 +2,30 @@ package com.uplus.qualityweb.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 @Slf4j
-@SpringBootTest
+@WebMvcTest(controllers = HelloController.class)
 class HelloControllerTest {
 
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    public void test1() {
+    public void test1() throws Exception {
         log.info("test ={}", true);
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
     }
 
 }
