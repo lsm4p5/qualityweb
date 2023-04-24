91da3de6ceff4307aca6bb64da4245d2a61b71a8
lsm4p5 <lsm4p5@gmail.com>
2023-04-24 23:49:40 +0900

    commit

--- src/main/java/com/uplus/qualityweb/web/HelloController.java
+++ src/main/java/com/uplus/qualityweb/web/HelloController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
+
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
